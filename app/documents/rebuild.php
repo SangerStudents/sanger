08a0556ca838455b3cc693cf0663e049b3ff12aa
Jonathan Reeve <jon.reeve@gmail.com>
2013-07-11 11:59:21 -0500

    added file moving to rebuild.php

--- app/documents/rebuild.php
+++ app/documents/rebuild.php
@@ -14,7 +14,7 @@ if ($_GET['verbose']) { //to enable debugging messages, add ?verbose=TRUE to the
 Make sure to back it up first!</h1>
 <p> </p> 
 <form action="<?php $PHP_SELF ?>" method="post" id="search1">  
-<input type="submit" name="submit" value="Search!"/>
+<input type="submit" name="submit" value="Rebuild!"/>
 </form> 
 
 
@@ -22,8 +22,8 @@ Make sure to back it up first!</h1>
 if ($_POST['submit']) { 
       echo '<p>Here we go. Fingers crossed.</p>'; 
       echo '<p>Now emptying database.</p>'; 
-      $tables = array('categories', 'correspondence', 'doctypes', 'documents', 'documents_category', 'journals', 'materials', 'mentioned_people', 'mentioned_places', 'ricordi_correspondence', 'test_cat'); 
-	foreach $tables as $table { 
+      $tables = array('categories', 'correspondence', 'documents', 'documents_category', 'journals', 'materials', 'mentioned_people', 'mentioned_places', 'ricordi_correspondence', 'test_cat'); 
+	foreach ($tables as $table) { 
  		 //make a query that truncates each 
 	      $emptyQuery = "TRUNCATE TABLE ".$table." ;";  
 
@@ -32,10 +32,33 @@ if ($_POST['submit']) {
 		}
 	      $result=mysql_query($emptyQuery) 
 		      or die ("Could not empty database."); 
-
+		if ($_GET['verbose']) { //to enable debugging messages, add ?verbose=TRUE to the URL, after search.php
+		}
 	} 
-      
+
+      echo '<p>Database now empty. Now attempting to move files.</p>';	
+      //following is adapted from http://stackoverflow.com/a/2082164 
+	    // Get array of all source files
+	    $files = scandir("../../xml_added");
+			if ($_GET['verbose']) { //to enable debugging messages, add ?verbose=TRUE to the URL, after search.php
+				echo 'Preparing to move files: '.$files; 
+			}
+	    // Identify directories
+	    $source = "../../xml_added/";
+	    $destination = "../../xml_queue/";
+	    // Cycle through all source files
+	    foreach ($files as $file) {
+	      if (in_array($file, array(".",".."))) continue;
+	      // If we copied this successfully, mark it for deletion
+	      if (copy($source.$file, $destination.$file)) {
+		$delete[] = $source.$file;
+	      }
+	    }
+	    // Delete all successfully-copied files
+	    foreach ($delete as $file) {
+	      unlink($file);
+	    } 
 } 
 
 include("../includes/footer.php");
-???
+?>

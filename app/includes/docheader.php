33994fa4118c0fa88f4cef409d3299a33d76652c
Jonathan Reeve <jon.reeve@gmail.com>
2013-07-31 12:08:19 -0500

    centered entire page, fixes #50

---
+++ app/includes/docheader.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+        "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<head>
+	<title>The Public Papers of Margaret Sanger: Web Edition</title> 
+	<meta http-equiv="content-type" content="text/html charset=ISO-8859-1" />
+	<link href="../documents/styles.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="../documents/global.js"></script> 
+</head>
+<body onload="MM_preloadImages('../images/header_r2_c1_f2.gif','../images/header_r2_c2_f2.gif','../images/header_r2_c3_f2.gif','../images/header_r2_c4_f2.gif','../images/header_r2_c5_f2.gif');">
+<div id="container" style="width: 626px; margin: 0px auto;"> 
+<p style="margin-top: 0px; margin-bottom: 0px;">
+	<img id="header_r1_c1" src="../images/header_r1_c1.gif" width="625" height="95" alt="" /><img src="../images/spacer.gif" width="1" height="95" alt="" /> 
+</p>
+<p style="margin-top: 0px; /*\*/margin-top: -3px;/**/">
+	<a href="project.php" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c1','','../images/header_r2_c1_f2.gif',1);"><img id="header_r2_c1" src="../images/header_r2_c1.gif" width="104" height="20" alt="" /></a><a href="edition.php" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c2','','../images/header_r2_c2_f2.gif',1);"><img id="header_r2_c2" src="../images/header_r2_c2.gif" width="104" height="20" alt="" /></a><a href="search.php" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c3','','../images/header_r2_c3_f2.gif',1);"><img id="header_r2_c3" src="../images/header_r2_c3.gif" width="104" height="20" alt="" /></a><a href="http://www.nyu.edu/projects/sanger/" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c4','','../images/header_r2_c4_f2.gif',1);"><img id="header_r2_c4" src="../images/header_r2_c4.gif" width="104" height="20" alt="" /></a><a href="contact.php" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('header_r2_c5','','../images/header_r2_c5_f2.gif',1);"><img id="header_r2_c5" src="../images/header_r2_c5.gif" width="104" height="20" alt="" /></a><img id="header_r2_c6" src="../images/header_r2_c6.jpg" width="105" height="20" alt="" /><img src="../images/spacer.gif" width="1" height="20" alt="" /> 
+</p>
+<div class="outBox">
+	<div class="docBox">
---
+++ app/includes/footer.php
@@ -0,0 +1,13 @@
+  </div>
+</div>
+	<div><br /></div>
+
+	<p style="margin-top: 0px; margin-bottom: 0px;">
+		<a href="http://www.nyu.edu/projects/sanger/search/aboutmspp.html" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('sanger_foot_r1_c1','','../images/sanger_foot_r1_c1_f2.gif',1);"><img id="sanger_foot_r1_c1" src="../images/sanger_foot_r1_c1.gif" width="104" height="20" alt="" /></a><a href="http://www.nyu.edu/projects/sanger/search/abouted.html" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('sanger_foot_r1_c2','','../images/sanger_foot_r1_c2_f2.gif',1);"><img id="sanger_foot_r1_c2" src="../images/sanger_foot_r1_c2.gif" width="104" height="20" alt="" /></a><a href="search.php" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('sanger_foot_r1_c3','','../images/sanger_foot_r1_c3_f2.gif',1);"><img id="sanger_foot_r1_c3" src="../images/sanger_foot_r1_c3.gif" width="104" height="20" alt="" /></a><a href="http://www.nyu.edu/projects/sanger/" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('sanger_foot_r1_c4','','../images/sanger_foot_r1_c4_f2.gif',1);"><img id="sanger_foot_r1_c4" src="../images/sanger_foot_r1_c4.gif" width="104" height="20" alt="" /></a><a href="http://www.nyu.edu/projects/sanger/search/contact.html" onmouseout="MM_swapImgRestore();" onmouseover="MM_swapImage('sanger_foot_r1_c5','','../images/sanger_foot_r1_c5_f2.gif',1);"><img id="sanger_foot_r1_c5" src="../images/sanger_foot_r1_c5.gif" width="104" height="20" alt="" /></a><img id="sanger_foot_r1_c6" src="../images/sanger_foot_r1_c6.jpg" width="105" height="20" alt="" />
+	</p>
+	<p style="margin-bottom: 0px; margin-top: -1px; /*\*/margin-top: -4px;/**/">
+		<img src="../images/spacer.gif" width="545" height="1" alt="" /><a href="http://validator.w3.org/check/referer"><img src="../images/xhtml11.gif" alt="valid" /></a>
+	</p>
+</div> <!-- end of Container --> 
+</body>
+</html>
